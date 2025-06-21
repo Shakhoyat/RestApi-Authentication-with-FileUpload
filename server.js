36f0b7ac4e4fc5ae7d06a29054a026020db41580
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-21 19:13:55 +0600

    refactor register.ejs template structure for improved readability

--- server.js
+++ server.js
@@ -26,14 +26,18 @@ mongoose
     console.error("Failed to connect to MongoDB", err);
   });
 
-// Rendering ejs template
+// Rendering login.ejs template
 app.get("/", (req, res) => {
   res.render("login.ejs", { url: null });
 });
 
+//Rendering register.ejs template
+app.get("/register", (req, res) => {
+  res.render("register.ejs", { url: null });
+});
 // Multer storage config
 const storage = multer.diskStorage({
-  filename: function (req, file, cb) {
+  filename: function (_req, file, cb) {
     const uniqueSuffix = Date.now() + path.extname(file.originalname);
     cb(null, file.fieldname + "-" + uniqueSuffix);
   },
@@ -41,25 +45,43 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Mongoose schema/model
-const imageSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
   filename: String,
   public_id: String,
   imgUrl: String,
 });
-const File = mongoose.model("cloudinary", imageSchema);
+const User = mongoose.model("user", userSchema);
 
 // Upload endpoint
 app.post("/register", upload.single("file"), async (req, res) => {
-  const file = req.file.path;
-  const cloudinaryResponse = await cloudinary.uploader.upload(file, {
+  if (!req.file) {
+    return res.status(400).send("File is required");
+  }
+  const filePath = req.file.path;
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).send("All fields are required");
+  }
+  const cloudinaryResponse = await cloudinary.uploader.upload(filePath, {
     folder: "Nodejs101",
   });
-  await File.create({
+
+  // Creating a user in MongoDB
+
+  const db = await User.create({
+    name,
+    email,
+    password,
     filename: req.file.originalname,
     public_id: cloudinaryResponse.public_id,
     imgUrl: cloudinaryResponse.secure_url,
   });
-  res.render("register.ejs", { url: cloudinaryResponse.secure_url });
+  res.redirect("/");
+  // res.render("register.ejs", { url: cloudinaryResponse.secure_url });
 });
 
 app.listen(PORT, () => {
